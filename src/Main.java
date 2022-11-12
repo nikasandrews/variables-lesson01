9ab016d7e4b739f134d938710049d3fd9f13a5a6
andrey nikiforov <nikifortis@mail.ru>
2022-11-12 20:47:55 +0300

    Дз готово

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,36 @@
 public class Main {
    public static void main(String[] args) {
+      // Задача1
 
-      System.out.println("Variables!");
+
       var dog = 8.0;
       System.out.println(dog);
       var cat = 3.6;
       System.out.println(cat);
       var paper = 763789;
       System.out.println(paper);
+      // Задача2
       dog = dog + 4;
       System.out.println(dog);
       cat = cat + 4;
       System.out.println(cat);
       paper = paper + 4;
       System.out.println(paper);
+      // Задача3
       dog = dog - 3.5;
       System.out.println(dog);
       cat = cat - 1.6;
       System.out.println(cat);
       paper = paper - 7639;
       System.out.println(paper);
+      // Задача4
       var friends = 19;
       System.out.println(friends);
       friends = friends * 2;
       System.out.println(friends);
       friends = friends / 7;
       System.out.println(friends);
+      // Задача5
       var frog = 3.5;
       System.out.println(frog);
       frog = frog * 10.0;
@@ -34,6 +39,7 @@ public class Main {
       System.out.println(frog);
       frog = frog + 4;
       System.out.println(frog);
+      // Задача 6
       var oneBoxerWeight = 78.2;
       System.out.println(oneBoxerWeight);
       var twoBoxerWeight = 82.7;
@@ -44,6 +50,7 @@ public class Main {
       var greenBoxersWeight = 78.2;
       var differenceBoxersWeight = redBoxersWeight - greenBoxersWeight;
       System.out.println("разница веса " +differenceBoxersWeight );
+      //Задача 7
       var workingHours=640;
       var workingDay=8;
       var totalEmployees=workingHours/workingDay;
