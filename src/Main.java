ad50b87dcada57d180e5803dd286ec14855e929b
andrey nikiforov <nikifortis@mail.ru>
2022-11-13 10:16:57 +0300

    Исправленное ДЗ!

--- src/Main.java
+++ src/Main.java
@@ -1,66 +1,70 @@
 public class Main {
    public static void main(String[] args) {
-      // Задача1
+       // Задание1
+       var dog = 8.0;
+       System.out.println(dog);
+       var cat = 3.6;
+       System.out.println(cat);
+       var paper = 763789;
+       System.out.println(paper);
+       // Задание2
+       dog = dog + 4;
+       System.out.println(dog);
+       cat = cat + 4;
+       System.out.println(cat);
+       paper = paper + 4;
+       System.out.println(paper);
+       // Задание3
+       var dog1 = 8.0;
+       dog1 = dog1 - 3.5;
+       System.out.println(dog1);
+       cat=cat-1.6;
+       System.out.println(cat);
+       paper-=7639;
+       System.out.println(paper);
+       // Задача4
+       var friend=19;
+       System.out.println(friend);
+       friend=friend+2;
+       System.out.println(friend);
+       friend=friend/7;
+       System.out.println(friend);
+
+       //Задание 5
+       var frog=3.5;
+       System.out.println(frog);
+       frog=frog*10;
+       System.out.println(frog);
+       frog=frog/3.5;
+       System.out.println(frog);
+       frog=frog+4;
+       System.out.println(frog);
+       //Задание6
+       var boxer1Weight =78.2;
+       var boxer2Weight =82.7;
+       System.out.println("общий вес двух бойцов:"+(boxer1Weight+boxer2Weight));
+       System.out.println("разница весов двух бойцов:"+(boxer2Weight+boxer1Weight));
+
+       //Задание 7
+       System.out.println("разница весов двух бойцов(1 способ):"+(boxer2Weight-boxer1Weight));
+       System.out.println("разница весов двух бойцов(2 способ):"+(boxer2Weight % boxer1Weight));
+       //Задание8
+       var totalWorkingHours = 640;
+       var hoursPerDay = 8;
+       var totalEmployees = totalWorkingHours / hoursPerDay;
+       System.out.println("Всего работников в компании - " + totalEmployees + " человек");
+
+       totalEmployees = totalEmployees + 94;
+       totalWorkingHours = totalEmployees * hoursPerDay;
+       System.out.println("если в компании работает " + totalEmployees + "человек то всего " +totalWorkingHours+ " часов работыможет быть поделено между сотрудниками");
 
 
-      var dog = 8.0;
-      System.out.println(dog);
-      var cat = 3.6;
-      System.out.println(cat);
-      var paper = 763789;
-      System.out.println(paper);
-      // Задача2
-      dog = dog + 4;
-      System.out.println(dog);
-      cat = cat + 4;
-      System.out.println(cat);
-      paper = paper + 4;
-      System.out.println(paper);
-      // Задача3
-      dog = dog - 3.5;
-      System.out.println(dog);
-      cat = cat - 1.6;
-      System.out.println(cat);
-      paper = paper - 7639;
-      System.out.println(paper);
-      // Задача4
-      var friends = 19;
-      System.out.println(friends);
-      friends = friends * 2;
-      System.out.println(friends);
-      friends = friends / 7;
-      System.out.println(friends);
-      // Задача5
-      var frog = 3.5;
-      System.out.println(frog);
-      frog = frog * 10.0;
-      System.out.println(frog);
-      frog = frog / 3.5;
-      System.out.println(frog);
-      frog = frog + 4;
-      System.out.println(frog);
-      // Задача 6
-      var oneBoxerWeight = 78.2;
-      System.out.println(oneBoxerWeight);
-      var twoBoxerWeight = 82.7;
-      System.out.println(twoBoxerWeight);
-      var totalBoxerWeight = oneBoxerWeight + twoBoxerWeight;
-      System.out.println("общийвес боксёров " + totalBoxerWeight + " кг!");
-      var redBoxersWeight = 82.7;
-      var greenBoxersWeight = 78.2;
-      var differenceBoxersWeight = redBoxersWeight - greenBoxersWeight;
-      System.out.println("разница веса " +differenceBoxersWeight );
-      //Задача 7
-      var workingHours=640;
-      var workingDay=8;
-      var totalEmployees=workingHours/workingDay;
-      System.out.println("всего работников" +totalEmployees);
-      var peopleWorking=80;
-      var addedWorkers=94;
-      var totalWorkers=peopleWorking+addedWorkers;
-      System.out.println("стало работать"+totalWorkers);
-      totalWorkers=totalWorkers*workingDay;
-      System.out.println("поделено часов между сотрудниками"+totalWorkers);
+       
+
+
+
+
+
 
 
 
@@ -69,7 +73,7 @@ public class Main {
 
 
    }
-}
+     }
 
 
 
